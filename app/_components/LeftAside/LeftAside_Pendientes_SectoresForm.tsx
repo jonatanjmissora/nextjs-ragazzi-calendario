7804b715df64bc95121ecba967788ebd5f783e57
Jonatan Missora <jonatanjmissora@gmail.com>
2025-01-20 16:08:59 -0300

    hicimos el form como se debe en el LeftAsideMenu para crear un pago pendiente

--- app/_components/LeftAside/LeftAside_Pendientes_SectoresForm.tsx
+++ app/_components/LeftAside/LeftAside_Pendientes_SectoresForm.tsx
@@ -5,45 +5,59 @@ import { insertarPendienteAction } from "@/app/_lib/actions/pendientes.action"
 import { updateSectoresActualesAction } from "@/app/_lib/actions/sectores.action"
 import { PagoType } from "@/app/_lib/schema/pago.type"
 import { getActualDateStr } from "@/app/_lib/utils/getActualDate"
-import { useState } from "react"
+import { useActionState, useState } from "react"
 import toast from "react-hot-toast"
+import SubmitBtn from "../SubmitBtn"
+
+type RespType = {
+  success: boolean;
+  message: string;
+} | null
 
 export const Sectores = ({ rubro, sectores, setMainActualSector }: { rubro: string, sectores: string[], setMainActualSector: React.Dispatch<React.SetStateAction<string>> }) => {
 
   const [actualSector, setActualSector] = useState<string>("")
-  const [error, setError] = useState<string>("")
 
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    setError("")
-    if (!actualSector) {
-      setError("No seleccionó sector")
-      return
-    }
-    const formData = new FormData(e.currentTarget)
+  const [formState, formAction, isPending] = useActionState(async (prevState: RespType, formData: FormData) => {
     const newPendiente = Object.fromEntries(formData.entries()) as PagoType
+
+    if (!newPendiente.sector) {
+      return {
+        success: false,
+        message: "No hay sector seleccionado"
+      }
+    }
+
     newPendiente._id = newPendiente.vencimiento + "-" + newPendiente.rubro + "-" + newPendiente.sector
 
     // insertar en pagosPendientes
-    const resp = await insertarPendienteAction(newPendiente)
-    if (!resp.success) {
-      setError(resp.message)
-      return
+    const serverResp = await insertarPendienteAction(newPendiente)
+    if (!serverResp.success) {
+      return {
+        success: false,
+        message: "Error al insertar pago"
+      }
     }
 
     // actualizar el sector del menu
     const newSectores = sectores.filter(sector => sector !== newPendiente.sector)
     const respDelete = await updateSectoresActualesAction(rubro, newSectores)
     if (!respDelete.success) {
-      setError(respDelete.message)
-      return
+      return {
+        success: false,
+        message: "Error al quitar sector"
+      }
     }
 
     // cerrar el Dropdown menu
-    if (resp.success && respDelete.success)
-      toast.success(resp.message)
-    setMainActualSector("")
-  }
+    if (serverResp.success && respDelete.success) {
+      toast.success(serverResp.message)
+      setMainActualSector("")
+    }
+
+    return null
+
+  }, null)
 
 
   return (
@@ -63,15 +77,17 @@ export const Sectores = ({ rubro, sectores, setMainActualSector }: { rubro: stri
         }
       </div>
 
-      <form onSubmit={onSubmit} className="flex gap-1 pt-2">
+      <form action={formAction} className="flex gap-1 pt-2">
         <input type="hidden" name="rubro" defaultValue={rubro} />
         <input type="hidden" name="sector" defaultValue={actualSector} />
         <input className="w-6/12 bg-slate-500 py-1 rounded px-1" type="date" name="vencimiento" defaultValue={getActualDateStr()} />
         <input className="w-5/12 bg-slate-500 rounded text-center px-1" placeholder="monto" defaultValue={0} type="number" name="monto" onFocus={(e) => e.currentTarget.select()} />
-        <button className="w-1/12 rounded bg-blue-500" type="submit"><PlaySVG className="size-5" currentColor="#ccc" /></button>
+        <SubmitBtn isPending={isPending} className="rounded-lg border border-slate-300 hover:bg-gray-300 hover:border-gray-300 hover:text-slate-800 transition">
+          <PlaySVG className="size-5 text-inherit" currentColor="currentColor" />
+        </SubmitBtn>
       </form>
 
-      <p className="text-orange-700">{error}</p>
+      <p className="text-orange-700">{formState?.message}</p>
 
     </ul>
   )
